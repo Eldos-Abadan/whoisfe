a18b4732bc9819e704a4d49a242ffbdaa30f4739
sw21d039 <112365971+sw21d039@users.noreply.github.com>
2023-06-05 13:02:27 +0800

    Magnai CV

--- whoisfe/app/views.py
+++ whoisfe/app/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def homeView(request):
-    return render(request,"home/mycv.html")
+    return render(request,"week1/Magnai/index.html")
 
 
 def week1View(request):
-    return render(request,"week1/tm1.html")
+    return render(request,"week1/Magnai/index.html")
+
