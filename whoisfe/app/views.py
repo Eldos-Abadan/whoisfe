6d8660bfa0bce128cbc7606792d5aecdbaa43833
tuguldur <tuugii.tuugii38@yahoo.com>
2023-06-05 15:13:54 +0800

    1

--- whoisfe/app/views.py
+++ whoisfe/app/views.py
@@ -6,11 +6,7 @@ def homeView(request):
     return render(request,"week1/Magnai/index.html")
 
 
-<<<<<<< Updated upstream
 # def week1View(request):
 #     return render(request,"week1/Magnai/index.html")
-=======
-
->>>>>>> Stashed changes
 def week1View(request):
     return render(request,"week1/Ha.Tuguldur/webcv.html")
