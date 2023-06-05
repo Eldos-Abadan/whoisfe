e089d3ef36418b10222728b23dd077250f3528b0
tuguldur <tuugii.tuugii38@yahoo.com>
2023-06-05 14:51:47 +0800

    tuguldur

--- whoisfe/app/views.py
+++ whoisfe/app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 
 def homeView(request):
     return render(request,"week1/Magnai/index.html")
 
 
+# def week1View(request):
+#     return render(request,"week1/Magnai/index.html")
 def week1View(request):
-    return render(request,"week1/Magnai/index.html")
-
+    return render(request,"week1/Ha.Tuguldur/webcv.html")
