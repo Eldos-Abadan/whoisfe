cf79a427c91f2ad82c016f38338e41e8bfcfa52d
zndra <112457204+zndra@users.noreply.github.com>
2023-06-05 13:57:33 +0800

    magnaiexmp

--- whoisfe/app/views.py
+++ whoisfe/app/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def homeView(request):
-    return render(request,"week1/Magnai/index.html")
+    return render(request,"home/mycv.html")
 
 
 def week1View(request):
-    return render(request,"week1/Magnai/index.html")
+    return render(request,"week1/tm1.html")
 
+def magnai(request):
+    return render(request,"week1/Magnai/index.html")
--- whoisfe/whoisfe/urls.py
+++ whoisfe/whoisfe/urls.py
@@ -2,7 +2,8 @@
 from django.urls import path
 from app import views
 
-urlpatterns = [    
-    path("",views.homeView,name="homeView"),
-    path("week1/",views.week1View,name="week1View"),
+urlpatterns = [
+    path("", views.homeView, name="homeView"),
+    path("week1/", views.week1View, name="week1View"),
+    path("week1/magnai/", views.magnai, name="magnai"),
 ]
