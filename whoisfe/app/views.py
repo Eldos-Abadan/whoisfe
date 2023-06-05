f1a02f843517161fa34cb7839f6a50b5596e924c
levi620 <115704743+levi620@users.noreply.github.com>
2023-06-05 15:36:09 +0800

    ebe
    cv gee django bolgow

--- whoisfe/app/views.py
+++ whoisfe/app/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def homeView(request):
-    return render(request,"week1/bumaa/cv.html")
+    return render(request,"week1/ebe/Ebindegada CV.html")
 
 
 def week1View(request):
-    return render(request,"week1/bumaa/cv.html")
+    return render(request,"week1/ebe/Ebindegada CV.html")
 
