70ecde945a978f772f5ffe44a37d3c49bc9679d6
sarnai0714 <112599590+sarnai0714@users.noreply.github.com>
2023-06-14 16:09:34 +0800

    sign up

--- whoisfe/whoisfe/route/urlsLogged.py
+++ whoisfe/whoisfe/route/urlsLogged.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from app import viewsLogged
 urlpatterns = [
-    # path("", viewsLogged., name="...View"),    
+    path("loggedViews/", viewsLogged.loginhomelogViews, name="loggedViews"), 
     ]   
