23023e10fc56ea5a2116f2e8f441db43ffc4cc06
uprogr <uprogr@gmail.com>
2023-06-15 14:48:52 +0800

    session test

--- whoisfe/app/viewsMain.py
+++ whoisfe/app/viewsMain.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
 def homeView(request):
-    return render(request, "home/mycv.html")
+    myData = {}
+    shineerUussen = "No"    
+    if "tooluur" not in request.session:
+        shineerUussen = "Yes"
+        request.session['tooluur'] = 0
+    else:
+        request.session['tooluur'] = request.session['tooluur'] + 1
+    
+
+
+    myData["tooluur"] = request.session['tooluur']
+    myData["shineerUussen"] = shineerUussen
+    return render(request, "home/mycv.html",myData)
