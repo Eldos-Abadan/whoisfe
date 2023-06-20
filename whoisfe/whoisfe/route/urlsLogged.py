d963ae3384362208c7cbeec1febb6dc10feb32a5
IS22D011 <135296437+IS22D011@users.noreply.github.com>
2023-06-20 15:02:25 +0800

    k

--- whoisfe/whoisfe/route/urlsLogged.py
+++ whoisfe/whoisfe/route/urlsLogged.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from app import viewsCreateCV, viewsDashboard, viewsMyNC, viewsGuide, viewsProfile
+from app import viewsCreateCV, viewsDashboard, viewsMyNC, viewsGuide, viewsProfile, viewsMain
 urlpatterns = [
     # path("createNC/", viewsCreateNC.createNCViews, name="createNCViews"),
     path("createCV/", viewsCreateCV.createCVViews, name="createCVViews"),
@@ -7,4 +7,5 @@ urlpatterns = [
     path("myNC/", viewsMyNC.myNCViews, name="myNCViews"),
     path("guide/", viewsGuide.guideViews, name="guideViews"),
     path("profile/", viewsProfile.profileViews, name="profileViews"),
+    path("home/", viewsMain.homeLogoutView, name="homeLogoutView"),
     ]   
