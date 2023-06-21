8aae3c335329025a3814c0cd481e1462af03c9a1
IS22D011 <135296437+IS22D011@users.noreply.github.com>
2023-06-21 09:15:44 +0800

    k

--- whoisfe/whoisfe/route/urlsNotLogin.py
+++ whoisfe/whoisfe/route/urlsNotLogin.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from app import viewsMain, viewsLogin, viewsRegister, viewsForget, viewsGuide
 urlpatterns = [  
-    path("", viewsMain.homeView, name="homeView"),
-    path("login/", viewsLogin.loginViews, name="loginViews"),
-    path("register/", viewsRegister.registerViews, name="registerViews"), 
-    path("forget/", viewsForget.forgetView, name="forgetView"),
-    path("guide/", viewsGuide.guideViews, name="guideViews"),
+    path("",            viewsMain.homeView,          name="homeView"),
+    path("login/",      viewsLogin.loginViews,       name="loginViews"),
+    path("register/",   viewsRegister.registerViews, name="registerViews"), 
+    path("forget/",     viewsForget.forgetView,      name="forgetView"),
+    path("guide/",      viewsGuide.guideViews,       name="guideViews"),
     ]   
