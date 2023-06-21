e83aebe49ba79b7f4d88e6b39b836bee5eee01c0
Odonkhuu <135576836+Odonkhuu@users.noreply.github.com>
2023-06-21 19:51:19 +0800

    fef

--- whoisfe/app/forget.py
+++ whoisfe/app/forget.py
@@ -1,32 +1,29 @@
-from    django.shortcuts    import render,redirect
-from    django.contrib      import messages
-from    whoisfe.settings    import *
-import  requests 
-import  hashlib
-import  json
+# from    django.shortcuts    import render,redirect
+# from    django.contrib      import messages
+# from    whoisfe.settings    import *
+# import  requests 
+# import  hashlib
+# import  json
 
 
-def mandakhHash(password):
-    return hashlib.md5(password.encode('utf-8')).hexdigest()
+# def mandakhHash(password):
+#     return hashlib.md5(password.encode('utf-8')).hexdigest()
 
 
-# nuuts vg sergeeh hesgiin
-def forgetView(request):
-    if request.method == "POST":
-        jsons = request.POST.dict()
-        message={}
-        email = json["myName"]
-        newPass1 = mandakhHash(json["myPass1"])
-        newPass2 = mandakhHash(json["myPass2"])
-        requestJSON = {
-            "email": email,
-            "newPass1": newPass1,
-            "newPass2": newPass2
-        }
-        r = requests.get("http://whoisb.mandakh.org/userLogin/",
-                            data=json.dumps(requestJSON),
-                            headers={'Content-Type': 'application/json'})
-        # messages.error(request, message)
-        # return render(request, 'register/register.html')
-                    
-####################################
+# # nuuts vg sergeeh hesgiin
+# def forgetView(request):
+#     if request.method == "POST":
+#         jsons = request.POST.dict()
+#         message={}
+#         email = json["myName"]
+#         newPass1 = mandakhHash(json["myPass1"])
+#         newPass2 = mandakhHash(json["myPass2"])
+#         requestJSON = {
+#             "email": email,
+#             "newPass1": newPass1,
+#             "newPass2": newPass2
+#         }
+#         r = requests.get("http://whoisb.mandakh.org/userLogin/",
+#                             data=json.dumps(requestJSON),
+#                             headers={'Content-Type': 'application/json'})
+# ####################################
--- whoisfe/app/viewsForget.py
+++ whoisfe/app/viewsForget.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from    django.shortcuts    import render,redirect
+from    django.contrib      import messages
+from    whoisfe.settings    import *
+import  requests 
+import  hashlib
+import  json
+
+def mandakhHash(password):
+    return hashlib.md5(password.encode('utf-8')).hexdigest()
 
 
-
+# nuuts vg sergeeh hesgiin
 def forgetView(request):
+    if request.method == "POST":
+        jsons = request.POST.dict()
+        message={}
+        email = jsons["myName"]
+        newPass1 = mandakhHash(jsons["myPass1"])
+        newPass2 = mandakhHash(jsons["myPass2"])
+        # print(newPass1)
+        requestJSON = {
+            "email": email,
+            "newPass1": newPass1,
+            "newPass2": newPass2
+        }
+        r = requests.get("http://whoisb.mandakh.org/userLogin/",
+                            data=json.dumps(requestJSON),
+                            headers={'Content-Type': 'application/json'})
+    
     return render(request, "forget/Forget.html")
-
-
+####################################
+# def forgetView(request):
+    
