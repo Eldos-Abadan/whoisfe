beb53635058b5cb79214743fb6b6e46b66035b6c
sarnai0714 <112599590+sarnai0714@users.noreply.github.com>
2023-06-26 11:13:34 +0800

    sa

--- whoisfe/app/viewsProfile.py
+++ whoisfe/app/viewsProfile.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render,redirect
-from whoisfe.settings import *
+from django.shortcuts import render #redirect
+# from whoisfe.settings import *
 
 def profileViews(request):
-    checkSession(request)  
-    if request.session['beegii'] ==0:        
-        return redirect("homeView")    
-    htmlRuuDataDamjuulah = {}
-    htmlRuuDataDamjuulah = request.session['userData']
-    return render(request, "Profile/1.html",htmlRuuDataDamjuulah)
+    # checkSession(request)  
+    # if request.session['beegii'] ==0:        
+    #     return redirect("homeView")    
+    # htmlRuuDataDamjuulah = {}
+    # htmlRuuDataDamjuulah = request.session['userData']
+    return render(request, "Profile/old_1.html") #htmlRuuDataDamjuulah
