72b5291045aca0adc7db9dbe1fede616f26e503f
Odonkhuu <is22d009@mandakh.edu.mn>
2023-07-06 13:41:47 +0800

    mycv section

--- whoisfe/app/viewsMy.py
+++ whoisfe/app/viewsMy.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from whoisfe.settings import *
+import requests
+import json
 
 def myCVViews(request):
-    return render(request, "my/myCV.html")
+    # Хэрэглэгч нэвтэрсэн эсэхийг шалгах
+    checkSession(request)
+    if request.session['beegii'] == 0:
+        return redirect("homeView")
+    htmlRuuDamjuulahUtguud = {}
+    htmlRuuDamjuulahUtguud["responseText"] = ""
+    # Хэрэглэгчийн нэр авах start
+    requestJSON = { "user_id": request.session['userId']}
+    r = requests.get("http://whoisb.mandakh.org/getDashboardInfo/",
+                        data=json.dumps(requestJSON),
+                        headers={'Content-Type': 'application/json'})
+    data = r.json()
+    userName = data["userName"]
+    # Хэрэглэгчийн нэр авах end
+    # Бүх загварийн мэдээлэл авах start
+    requestJSON = {
+        "user_id": request.session['userId']
+    }
+    r = requests.get("http://whoisb.mandakh.org/tempList/",
+                        data=json.dumps(requestJSON),
+                        headers={'Content-Type': 'application/json'})
+    data = r.json()
+    # Бүх загварийн мэдээлэл авах end
+    
+    kholboos = []
+    for ele in data:
+        # diki = 
+        kholboos.append({
+            "name": str(ele["name"]), 
+            "id": str(ele["id"])
+        })
+    htmlRuuDamjuulahUtguud["userName"] = userName
+    htmlRuuDamjuulahUtguud["kholboos"] = kholboos
+    return render(request, "my/myCV.html", htmlRuuDamjuulahUtguud)
 
 def myNCViews(request):
     return render(request, "my/myNC.html")
