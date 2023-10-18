c5f35d261e0385bde5e2c29ac54d904de5de78ab
IS22D011 <135296437+IS22D011@users.noreply.github.com>
2023-10-18 17:05:55 +0800

    Update urlsLogged.py

--- whoisfe/whoisfe/route/urlsLogged.py
+++ whoisfe/whoisfe/route/urlsLogged.py
@@ -17,7 +17,6 @@ urlpatterns = [
     path("profile/skill/" ,                 viewsProfile.profileSkill,      name="profileSkill"     ) ,
     path("profile/social/",                 viewsProfile.profileSocial,     name="profileSocial"    ) ,
     path("profile/social/del/<int:id>/" ,   viewsProfile.profileSocialDel,  name="profileSocialDel" ) ,
-    path('upload/<int:id>/',                viewsProfile.upload_file,       name='upload_file'      ),
     ##############################################################################
     path("home/"     ,   viewsMain.homeLogoutView,        name="homeLogoutView") ,
     path("wallet/"   ,   viewsMain.walletView,            name="wallet"        ) ,
