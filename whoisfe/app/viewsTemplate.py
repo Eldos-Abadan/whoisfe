8ac5e65f0371dcce934128a3c3fe5d5647ea8570
Tudu666 <135106264+Tudu666@users.noreply.github.com>
2023-10-25 06:24:47 -0700

    za mdkue zasna

--- whoisfe/app/viewsTemplate.py
+++ whoisfe/app/viewsTemplate.py
@@ -2,44 +2,75 @@ from django.shortcuts import render,redirect
 from whoisfe.settings import *
 import requests
 import json
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 ######## cvView ######################################################
-def myCvView(request,tid,uname):    
-    
-    requestJSON = {
-        "id": tid
-    }
-    r = requests.get("http://whoisb.mandakh.org/tempGet/",
-                        data=json.dumps(requestJSON),
-                        headers={'Content-Type': 'application/json'})    
-    try:
-        
-        responseJson = r.json()        
-        if responseJson["responseCode"] == 200:           
-           renderFile = responseJson["tempData"][0]["file"]
-        else:
-           return render(request, templateErrorRenderHTML)
-    except:        
-      return render(request, templateErrorRenderHTML)
-    requestJSON = {
-        "userName": uname
-    }
-    r = requests.get("http://whoisb.mandakh.org/userAllInfo/",
-                        data=json.dumps(requestJSON),
-                        headers={'Content-Type': 'application/json'})    
+from django.http import HttpResponse
+import os
+
+
+def myCvView(request, tid, uname):
+    requestJSON = {"id": tid}
+    r = requests.get("http://whoisb.mandakh.org/tempGet/", data=json.dumps(requestJSON),
+                     headers={'Content-Type': 'application/json'})
     try:
         responseJson = r.json()
-        
         if responseJson["responseCode"] == 200:
-           HTMLruuDamjuulahUtga = responseJson
+            renderFile = responseJson["tempData"][0]["file"]
         else:
-           return render(request, templateErrorRenderHTML)
-    except:        
-      return render(request, templateErrorRenderHTML)
-    
-    return render (request, renderFile, HTMLruuDamjuulahUtga)
+            return render(request, "templateErrorRenderHTML.html")
+    except:
+        return render(request, "templateErrorRenderHTML.html")
+
+    requestJSON = {"userName": uname}
+    if request.method == "POST" and "tempSubmit" in request.POST:
+        serviceHayag = "http://whoisb.mandakh.org/uploadTemplate/"
+        name = request.POST.get("name")
+        tempTypeId = int(request.POST.get("tempTypeId"))
+        catId = int(request.POST.get("catId"))
+        file = request.POST.get("file")
+        requestJSON = {
+            "name": name,
+            "tempTypeId": tempTypeId,
+            "catId": catId,
+            "file": file
+        }
+        if "file" in request.FILES:
+            uploaded_file = request.FILES["file"]
+            if uploaded_file:
+                file_system = FileSystemStorage(location=settings.templateVar+'/newTemplates')
+                filename = file_system.save(uploaded_file.name, uploaded_file)
+                requestJSON["file"] = f'templates/newTemplates/{filename}'
+                print(requestJSON["file"])
+
+        r = requests.get(serviceHayag, data=json.dumps(requestJSON), headers={'Content-Type': 'application/json'})
+        responseJson = r.json()
+        HTMLruuDamjuulahUtga = {"responseText": responseJson["responseText"]}
+        if responseJson["responseCode"] == 200:
+            HTMLruuDamjuulahUtga["textColor"] = "#00ff00"
+        else:
+            HTMLruuDamjuulahUtga["textColor"] = "#ff0000"
+
+    r = requests.get("http://whoisb.mandakh.org/userAllInfo/", data=json.dumps(requestJSON),
+                     headers={'Content-Type': 'application/json'})
+    try:
+        responseJson = r.json()
+
+        if responseJson["responseCode"] == 200:
+            HTMLruuDamjuulahUtga = responseJson
+        else:
+            return render(request, "myCV")
+    except:
+        return render(request, "myCV")
+
+    return render(request, renderFile, HTMLruuDamjuulahUtga)
+
 ###########################################################################
 
 ########### ncView ######################################################
+
+
+
 def myNcView(request,tid,uname):    
     
     requestJSON = {
